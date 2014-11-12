0d281bfaf141264d62ed402b2f859e9b92fc053e
Augustin Cavalier <waddlesplash@gmail.com>
2014-11-12 15:24:36 -0500

    Add a script that can fetch Hugo.

---
+++ getHugo.sh
@@ -0,0 +1,4 @@
+HUGO_VERSION=31
+wget http://www.ifarchive.org/if-archive/programming/hugo/source/hugov${HUGO_VERSION}_source.tar.gz
+tar -xvf hugov${HUGO_VERSION}_source.tar.gz
+rm hugov${HUGO_VERSION}_source.tar.gz
